6ae72de8b3aa01ad3b1484db1b6c78929f667c1f
SimranSeenundun <s2025908@ed.ac.uk>
2022-09-25 17:44:08 +0100

    Completed a and c

--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -1,4 +1,23 @@
 package uk.ac.ed.inf;
 
 public class LngLat {
+    public double lng, lat;
+    public LngLat(double longitude, double latitude)    {
+        lng = longitude;
+        lat = latitude;
+    }
+
+    public boolean inCentralArea()  {
+        return false;
+    }
+
+    public double distanceTo(LngLat lnglat) {
+        double x1 = lng;
+        double x2 = lnglat.lng;
+        double y1 = lat;
+        double y2 = lnglat.lat;
+
+        return Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2));
+    }
 }
+
