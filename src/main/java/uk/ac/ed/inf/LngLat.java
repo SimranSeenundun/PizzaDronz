96fd6bc8b4406a270ad7b9aea5fafcccd6da7f90
SimranSeenundun <s2025908@ed.ac.uk>
2022-09-28 15:59:40 +0100

    Completed a and c and d?

--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -2,6 +2,11 @@ package uk.ac.ed.inf;
 
 public class LngLat {
     public double lng, lat;
+    public int north = 90;
+    public int south = 270;
+    public int east = 0;
+    public int west = 180;
+
     public LngLat(double longitude, double latitude)    {
         lng = longitude;
         lat = latitude;
@@ -19,5 +24,16 @@ public class LngLat {
 
         return Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2));
     }
+
+    public boolean closeTo(LngLat lnglat)  {
+       if ((distanceTo(lnglat) <= 0.00015) && (distanceTo(lnglat) != 0)) {
+           return true;
+       }
+       return false;
+    }
+
+    //public double nextPosition(int compDirection) {
+        //compDirection =
+    //}
 }
 
